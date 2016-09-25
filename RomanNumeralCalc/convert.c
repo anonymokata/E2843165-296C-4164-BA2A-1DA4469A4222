72573717c795f1d9671875812bc72ed028c8a98d
anonymous <anonymous>
2016-09-25 17:10:00 -0400

    convert passes the return values for all single numerals

--- RomanNumeralCalc/convert.c
+++ RomanNumeralCalc/convert.c
@@ -4,6 +4,7 @@
  *  Created on: Sep 25, 2016
  *      Author: brad
  */
+#include "stdlib.h"
 #include "checkSyntax.h"
 #include "convert.h"
 
@@ -23,5 +24,18 @@ int singleNumeralValue(char numeral)
 	{
 		case 'I' :
 			return 1;
+		case 'V' :
+			return 5;
+		case 'X' :
+			return 10;
+		case 'L' :
+			return 50;
+		case 'C' :
+			return 100;
+		case 'D' :
+			return 500;
+		case 'M' :
+			return 1000;
+
 	}
 }
