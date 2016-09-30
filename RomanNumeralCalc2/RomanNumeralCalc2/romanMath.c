dee0108fc1f9c607808967c11fc4093a328f65be
anonymous <anonymous>
2016-09-29 21:54:29 -0400

    altered makefile, added romanMathTest.check, successful compile, 1st test fails.

---
+++ RomanNumeralCalc2/RomanNumeralCalc2/romanMath.c
@@ -0,0 +1,14 @@
+/*
+ * romanMath.c
+ *
+ *  Created on: Sep 29, 2016
+ *      Author: brad
+ */
+#include <stdio.h>
+#include "convert.h"
+
+char *add(char *a, char *b)
+{
+	return "II";
+}
+
---
+++ RomanNumeralCalc2/RomanNumeralCalc2/romanMath.h
@@ -0,0 +1,13 @@
+/*
+ * romanMath.h
+ *
+ *  Created on: Sep 29, 2016
+ *      Author: brad
+ */
+
+#ifndef ROMANMATH_H_
+#define ROMANMATH_H_
+
+char *add(char *a, char *b);
+
+#endif /* ROMANMATH_H_ */
