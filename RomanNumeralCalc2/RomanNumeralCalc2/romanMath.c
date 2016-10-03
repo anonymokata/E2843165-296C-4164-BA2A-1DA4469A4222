69f0926fe675103f0e960a95f06839f51339f89f
anonymous <anonymous>
2016-10-02 22:41:12 -0400

    passes test to detect sum which exceeds allowable maximum

--- RomanNumeralCalc2/RomanNumeralCalc2/romanMath.c
+++ RomanNumeralCalc2/RomanNumeralCalc2/romanMath.c
@@ -12,7 +12,10 @@ char *add(char *a, char *b)
 {
 	int termA = convertRomanNumeralStringToBaseTenInt(a);
 	int termB = convertRomanNumeralStringToBaseTenInt(b);
-	return convertIntToRomanNumeralString(termA + termB);
+	int sum = termA + termB;
+	if (sum < 4000)
+		return convertIntToRomanNumeralString(sum);
+	return NULL;
 }
 
 char *sub(char *a, char *b)
